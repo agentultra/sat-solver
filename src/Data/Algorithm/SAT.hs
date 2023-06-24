8affe8e0e845cf5a131e5fc7ad3350f571fc40aa
Simon Hudon <simon.hudon@gmail.com>
2023-06-24 13:23:45 -0400

    define formula and evaluation

--- src/Data/Algorithm/SAT.hs
+++ src/Data/Algorithm/SAT.hs
@@ -1,1 +1,16 @@
-module SAT where
+module Data.Algorithm.SAT where
+
+import Data.Map as M
+
+newtype Var = Var Int
+  deriving (Eq, Ord)
+
+newtype Formula = Formula [[Var]]
+
+newtype Assignment = Assgn (Map Var Bool)
+
+fetch :: Assignment -> Var -> Maybe Bool
+fetch (Assgn m) v = v `M.lookup` m
+
+eval :: Assignment -> Formula -> Maybe Bool
+eval asgn (Formula fs) = and <$> mapM (fmap or . mapM (fetch asgn)) fs
