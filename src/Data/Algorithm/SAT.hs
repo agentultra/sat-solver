b0171cb09b208aaeb75986ffad6d98c3fc767fc4
Simon Hudon <simon.hudon@gmail.com>
2023-06-24 23:02:02 -0400

    implement pure literal elimination. Lacking: test

--- src/Data/Algorithm/SAT.hs
+++ src/Data/Algorithm/SAT.hs
@@ -1,13 +1,14 @@
 module Data.Algorithm.SAT where
 
 import Control.Monad.State
-import Lens.Micro
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import Data.Maybe
+import Data.Monoid
+import Lens.Micro
 
 newtype Var = Var Int
-  deriving (Eq, Ord)
+  deriving (Eq, Ord, Show)
 
 data Lit = Pos Var | Neg Var
 
@@ -16,8 +17,10 @@ data Lit = Pos Var | Neg Var
 --   a clause is disjunction of literals
 --   a literal is a variable with or without negation
 newtype Formula = Formula [Map Var Bool]
+  deriving Show
 
 newtype Assignment = Assgn (Map Var Bool)
+  deriving Show
 
 assignment :: Lens' Assignment (Map Var Bool)
 assignment f (Assgn m) = Assgn <$> f m
@@ -48,7 +51,12 @@ isUnitClause clause
     mkLit v False = Neg v
 
 varOccurrences :: Formula -> Map Var (Int, Int)
-varOccurrences (Formula fs) = undefined
+varOccurrences (Formula fs) =
+  M.map (both %~ getSum) . M.unionsWith (<>) . map (M.map countOne) $ fs
+  where
+    countOne :: Bool -> (Sum Int, Sum Int)
+    countOne True = (1, 0)
+    countOne False = (0, 1)
 
 -- in x /\ ... x is a unit clause
 unitClausePropagation :: Formula -> M Formula
@@ -59,9 +67,24 @@ unitClausePropagation (Formula fs) = do
       Nothing -> return $ Just clause
   return $ Formula $ catMaybes fs'
 
+-- a pure literal is when a variable appears only as a positive literal or a negative literal
 pureLitElimination :: Formula -> M Formula
-pureLitElimination (Formula fs) = undefined
+pureLitElimination f@(Formula fs) = do
+    M $ modify $ assignment %~ insertLit
+    return $ Formula fs'
+  where
+    isPure (p, n) = p == 0 || n == 0
+    pureLit = M.filter isPure $ varOccurrences f
+    isPosLit (_, n) = n == 0
+    insertLit m = M.map isPosLit pureLit `M.union` m
+    fs' = filter (\c -> M.null (M.intersection c pureLit)) fs
 
+instance Show a => Show (M a) where
+  show (M a) = show (runState a (Assgn M.empty))
+
+
+-- property base tests: running pureLitElimination gets us an
+-- evaluation that is no less defined that before
 
 -- (x ∨ y ∨ z) ∧ (x ∨ ¬ y ∨ z) ∧ (y ∨ ¬ z)
 -- x := true
