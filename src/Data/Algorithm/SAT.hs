7b3080790cccd0a76f9c66cdba46b26c7bb04d5d
Simon Hudon <simon.hudon@gmail.com>
2023-06-29 11:05:00 -0400

    add example using the assignment building primitives

--- src/Data/Algorithm/SAT.hs
+++ src/Data/Algorithm/SAT.hs
@@ -26,11 +26,16 @@ mkLit :: Bool -> Var -> Lit
 mkLit True  v = Pos v
 mkLit False v = Neg v
 
+fromLit :: Lit -> (Var, Bool)
+fromLit (Pos v) = (v, True)
+fromLit (Neg v) = (v, False)
+
 -- in conjunctive normal form:
 --   a formula is a conjunction of clauses
 --   a clause is disjunction of literals
 --   a literal is a variable with or without negation
-newtype Formula = Formula [Map Var Bool]
+newtype Formula = Formula { getClauses :: [Map Var Bool] }
+  deriving (Eq, Ord)
   -- deriving Show
 
 newtype Assignment = Assgn (Map Var Bool)
@@ -56,19 +61,19 @@ eval :: Assignment -> Formula -> Maybe Bool
 eval asgn (Formula fs) = and <$> mapM (fmap or . M.traverseWithKey (test asgn)) fs
 
 conj :: Formula -> Formula -> Formula
-conj (Formula xs) (Formula ys) = Formula $ xs ++ ys
+conj (Formula xs) (Formula ys) = mkFormula $ xs ++ ys
 
 disjClause :: Map Var Bool -> Map Var Bool -> Maybe (Map Var Bool)
 disjClause mx my = do
   let mz = M.mergeWithKey (\_k x y -> guard (x /= y))
             (const M.empty) (const M.empty) mx my
   let mz' = mx `M.union` my
-  guard (not (M.null mz) || M.null mz')
+  guard (M.null mz || M.null mx || M.null my)
   return mz'
 
 disj :: Formula -> Formula -> Formula
 disj (Formula xs) (Formula ys) =
-  Formula $ Ord.nub $ catMaybes [ disjClause x y | x <- xs, y <- ys ]
+  Formula $ Ord.nubSort $ catMaybes [ disjClause x y | x <- xs, y <- ys ]
 
 data FmlAssgnPair = FAP Formula Assignment
 
@@ -77,6 +82,9 @@ genClause n = do
   l <- forM [0..n] (\i ->
     fmap (Var i,) <$> arbitrary)
   return $ M.fromList $ catMaybes l
+
+fromListLit :: [[Lit]] -> Formula
+fromListLit xs = mkFormula $ M.fromList . map fromLit <$> xs
 
 instance Arbitrary Var where
   arbitrary = Var <$> arbitrary
@@ -128,13 +136,38 @@ prop_disj n =
   counterexample ("f `disj` f' == " ++ show (f `disj` f')) $
   eval sigma (f `disj` f') === ((||) <$> eval sigma f <*> eval sigma f')
 
+single_example f = do
+  f'  <- unitClausePropagation f
+  f'' <- pureLitElimination f'
+  return (f', f'')
+
+single_test =
+  let x0 = Var 0
+      x1 = Var 1
+      x2 = Var 2
+      f = fromListLit [ [Pos x0], [Neg x0, Pos x1, Pos x2], [Pos x1, Neg x2], [Pos x0, Pos x1, Pos x2] ]
+      ((f', f''), a) = runSolver (single_example f) in
+      -- (f', a) = runSolver (unitClausePropagation f)
+    counterexample ("assignment: " ++ show a) $
+    counterexample ("f:   " ++ show f) $
+    counterexample ("f':  " ++ show f') $
+    counterexample ("f'': " ++ show f'') $
+    f === f'
+    -- insertLit m = M.map isPosLit pureLit `M.union` m
+    -- fs' = filter (\c -> M.null (M.intersection c pureLit)) fs
+
+
 test_suite :: IO ()
 test_suite = do
   quickCheck prop_conj
   quickCheck prop_disj
+  quickCheck single_test
 
 newtype M a = M { runM :: State Assignment a }
   deriving (Functor,Applicative,Monad)
+
+runSolver :: M a -> (a, Assignment)
+runSolver m = runState (runM m) (Assgn M.empty)
 
 assign :: Var -> Bool -> M ()
 assign v b = M $ modify $ assignment %~ M.insert v b
@@ -146,19 +179,46 @@ satisfy (Neg v) = assign v False
 isUnitClause :: Map Var Bool -> Maybe Lit
 isUnitClause clause
   | M.size clause == 1 = uncurry (flip mkLit) <$> M.lookupMin clause
-  | otherwise = Nothing
+  | otherwise          = Nothing
 
 varOccurrences :: Formula -> Map Var (Int, Int)
 varOccurrences (Formula fs) =
   M.map (both %~ getSum) . M.unionsWith (<>) . map (M.map countOne) $ fs
   where
     countOne :: Bool -> (Sum Int, Sum Int)
-    countOne True = (1, 0)
+    countOne True  = (1, 0)
     countOne False = (0, 1)
+
+hasTrueLit :: Assignment -> Map Var Bool -> Bool
+hasTrueLit (Assgn a) cl =
+  not $ M.null $
+    M.filter id $
+    M.intersectionWith (==) a cl
+
+pruneFalseLit :: Assignment -> Formula -> Formula
+pruneFalseLit (Assgn asgn) (Formula f) =
+  mkFormula $
+    Ord.nubSort $
+    map (\c ->
+           M.differenceWith
+              (\b b' -> b <$ guard (b == b'))
+              c asgn) $ f
+
+-- run a function that updates the assignment and returns a formula
+-- and use the new assignments to simplify the resulting formula
+updateAssignment :: M Formula -> M Formula
+updateAssignment (M m) = M $ do
+  (Assgn asgn) <- get
+  put (Assgn M.empty)
+  Formula f <- m
+  a'@(Assgn asgn') <- get
+  put (Assgn $ asgn' `M.union` asgn)
+  let f' = filter (not . hasTrueLit a') f
+  return $ pruneFalseLit a' (Formula f')
 
 -- in x /\ ... x is a unit clause
 unitClausePropagation :: Formula -> M Formula
-unitClausePropagation (Formula fs) = do
+unitClausePropagation (Formula fs) = updateAssignment $ do
   fs' <- forM fs $ \clause ->
     case isUnitClause clause of
       Just l -> satisfy l >> return Nothing
@@ -167,7 +227,7 @@ unitClausePropagation (Formula fs) = do
 
 -- a pure literal is when a variable appears only as a positive literal or a negative literal
 pureLitElimination :: Formula -> M Formula
-pureLitElimination f@(Formula fs) = do
+pureLitElimination f@(Formula fs) = updateAssignment $ do
     M $ modify $ assignment %~ insertLit
     return $ Formula fs'
   where
